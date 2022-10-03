bd205fc2d0a1dbf6c1be7dd224ca397b10924a45
Dmitrii Larionov <dlarionov51@gmail.com>
2022-10-03 12:03:23 +0300

    FluentModel setters

--- FluentModel.cs
+++ FluentModel.cs
@@ -8,6 +8,18 @@ public abstract class FluentModel
     protected MobileObjectModel MobileObject { get; private set; }
     protected MapModel Map { get; private set; }
 
+    public FluentModel SetCorrectionModel(ICorrectionModel correctionModel)
+    {
+        SavedCorrectionModel = correctionModel;
+        return this;
+    }
+
+    public FluentModel SetPredictionModel(IPredictionModel predictionModel)
+    {
+        SavedPredictionModel = predictionModel;
+        return this;
+    }
+
     public virtual FluentModel Initialize<TInitModel>()
     {
         return this;
@@ -20,6 +32,7 @@ public abstract class FluentModel
         model.Apply(MobileObject, data);
         return this;
     }
+
     public FluentModel Predict(object data)
         => Predict(SavedPredictionModel, data);
 
