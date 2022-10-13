355da47e2a129dc2b92084f5c7413f7cf574462b
Dmitrii Larionov <dlarionov51@gmail.com>
2022-10-13 14:38:56 +0300

    GraphModel distance matrix calculation

--- Models/MapModels/GraphModel/GraphMap.cs
+++ Models/MapModels/GraphModel/GraphMap.cs
@@ -1,14 +1,74 @@
-﻿using NGenerics.DataStructures.General;
+﻿using QuickGraph;
+using QuickGraph.Algorithms;
+using QuickGraph.Algorithms.ShortestPath;
+using FluentSLAM.Misc;
+
+using FluentSLAM.MapModels.GraphModel;
 
 namespace FluentSLAM.MapModels
 {
-	public class GraphMap<TVertexData> : MapModel
+	public class GraphMap<TVertex, TEdge> : MapModel
+		where TVertex : notnull
+		where TEdge : IEdge<TVertex>
 	{
-		public Graph<TVertexData> Graph { get; private set; }
+		public IMutableUndirectedGraph<TVertex, TEdge> Graph { get; private set; }
 
-		public GraphMap(bool isDirected)
+		private Dictionary<TEdge, double> _edgeWeights = new Dictionary<TEdge, double>();
+
+
+        private Dictionary<TVertex, int> _vertexIndex;
+		private double[,] _distanceMatrix;
+
+		public GraphMap()
 		{
-			Graph = new Graph<TVertexData>(isDirected);
+			Graph = new UndirectedGraph<TVertex, TEdge>();
+		}
+
+        public GraphMap(List<TEdge> edgeList)
+        {
+            Graph = new UndirectedGraph<TVertex, TEdge>();
+
+			foreach (var edge in edgeList)
+				Graph.AddVerticesAndEdge(edge);
+
+			CalculateVerticesIndex();
+			CalculateDistanceMatrix();
+        }
+
+		private void CalculateVerticesIndex()
+		{
+			_vertexIndex = new Dictionary<TVertex, int>();
+			foreach (var (vertex, index) in Graph.Vertices.WithIndex())
+				_vertexIndex.Add(vertex, index);
+		}
+
+        private void CalculateDistanceMatrix()
+        {
+			_distanceMatrix = new double[_vertexIndex.Count, _vertexIndex.Count];
+
+			foreach (var v in Graph.Vertices)
+			{
+                var PathTryFunc = AlgorithmExtensions.ShortestPathsDijkstra<TVertex, TEdge>(Graph, (edge) => _edgeWeights[edge], v);
+
+				foreach (var u in Graph.Vertices)
+				{
+					IEnumerable<TEdge> result;
+					PathTryFunc(u, out result);
+					_distanceMatrix[_vertexIndex[v], _vertexIndex[u]] = result.Sum(e => _edgeWeights[e]);
+				}
+            }
+        }
+
+		public double GetDistanceBetweenVertices(TVertex source, TVertex target)
+		{
+			throw new NotImplementedException();
+		}
+
+		public double GetDistanceBetweenPoints(
+			EdgePoint<Edge<Vertex>, double> firstPoint,
+			EdgePoint<Edge<Vertex>, double> secondPoint)
+		{
+			throw new NotImplementedException();
 		}
 	}
 }
---
+++ Models/MapModels/GraphModel/Vertex.cs
@@ -0,0 +1,15 @@
+﻿namespace FluentSLAM.MapModels.GraphModel
+{
+	public struct Vertex
+	{
+		public readonly double X, Y, Z;
+
+		public Vertex(double x, double y, double z)
+		{
+			X = x;
+			Y = y;
+			Z = z;
+		}
+	}
+}
+
