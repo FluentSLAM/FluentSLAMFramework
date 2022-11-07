d3c24ba9d44e565fed35d2a69a1c7cf24f2878b9
Dmitrii Larionov <dlarionov51@gmail.com>
2022-11-07 12:13:00 +0300

    MultiGrid placeholder

---
+++ Models/MapModels/MultiGrid.cs
@@ -0,0 +1,42 @@
+﻿namespace FluentSLAM.MapModels
+{
+	public class MultiGrid<TKey, TGrid, TCell> : IGrid<TCell>
+		where TGrid : IGrid<TCell>
+		where TKey : notnull
+	{
+		private Dictionary<TKey, TGrid> _grid;
+
+		public TGrid this[TKey key]
+		{
+			get
+			{
+				return _grid[key];
+			}
+
+			set
+			{
+				_grid[key] = value;
+			}
+		}
+
+        public MultiGrid()
+		{
+			_grid = new Dictionary<TKey, TGrid>();
+		}
+
+		public TCell Sum()
+		{
+            throw new NotImplementedException();
+        }
+
+		public TCell Average()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Normalize()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
