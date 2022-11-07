6114ed710fce0c4b3ccb8ae8862ec020a733db36
Dmitrii Larionov <dlarionov51@gmail.com>
2022-11-07 12:12:20 +0300

    IGrid interface

--- Models/MapModels/IGrid.cs
+++ Models/MapModels/IGrid.cs
@@ -1,8 +1,8 @@
-﻿using System;
-namespace FluentSLAM.MapModels
+﻿namespace FluentSLAM.MapModels
 {
 	public interface IGrid<TCell>
 	{
+		public TCell Sum();
 		public TCell Average();
 		public void Normalize();
 	}
