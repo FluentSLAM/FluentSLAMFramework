387be55feb04ad2b2cb03de78646897a1d01ba79
Dmitrii Larionov <dlarionov51@gmail.com>
2022-12-06 11:00:47 +0300

    IParticleMotionModel is implemented with default method implementation

---
+++ Models/ParticleFilter/IParticleMotionModel.cs
@@ -0,0 +1,14 @@
+﻿namespace FluentSLAM.ParticleFilter
+{
+    public interface IParticleMotionModel<TParticle, TDataEntry> : IMotionModel<IMobileObjectModel, TDataEntry>
+        where TParticle : IParticle, IMobileObjectModel
+        where TDataEntry : struct
+    {
+        public void ApplyToSet(IParticleSet<TParticle> set, TDataEntry data)
+        {
+            foreach (var particle in set.Particles)
+                Apply(particle, data);
+        }
+    }
+}
+
