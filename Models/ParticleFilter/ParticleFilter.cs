a2485cffce8a42f49cd5890474f3bb51b29ed741
Dmitrii Larionov <dlarionov51@gmail.com>
2022-12-10 02:15:35 +0300

    Partial implementation of Particle filter is made and moved to its own folder

---
+++ Models/ParticleFilter/ParticleFilter.cs
@@ -0,0 +1,41 @@
+﻿using FluentSLAM.ParticleFilter;
+
+namespace FluentSLAM
+{
+	public class ParticleFilter<TParticle, TMap> : FluentModel<IParticleCollection<TParticle>, TMap>
+        where TParticle : IParticle, IMobileObjectModel, new()
+        where TMap : IMapModel
+	{
+        public ParticleFilter(IParticleCollection<TParticle> mobileObject, TMap map, int number) : base(mobileObject, map)
+        {
+            mobileObject.InitParticles(number);
+        }
+
+        protected override void ApplyMotionModel<TDataEntry>(
+            IMotionModel<IParticleCollection<TParticle>, TDataEntry> model,
+            TDataEntry data)
+        {
+            // TODO: parallel computing
+            model.Apply(MobileObject, data);
+        }
+
+        protected override void ApplyObservationModel<TDataEntry>(
+            IObservationModel<TMap, TDataEntry> model,
+            TDataEntry data)
+        {
+            model.Apply(Map, data);
+            RecalculateWeights();
+        }
+
+        protected virtual void RecalculateWeights()
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual ParticleFilter<TParticle, TMap> Resample()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
