4753f14741704835a5e3226dfd8b04f0abc53f48
Dmitrii Larionov <dlarionov51@gmail.com>
2022-12-10 02:14:55 +0300

    IParticleMotionModel is replced by ParticleCollectionMotionModel abstract class with some default implementations

---
+++ Models/ParticleFilter/ParticleCollectionMotionModel.cs
@@ -0,0 +1,19 @@
+﻿namespace FluentSLAM.ParticleFilter
+{
+    public abstract class ParticleCollectionMotionModel<TParticle, TDataEntry> : IMotionModel<IParticleCollection<TParticle>, TDataEntry>
+        where TParticle : IParticle, IMobileObjectModel
+        where TDataEntry : struct
+    {
+        public void Apply(IParticleCollection<TParticle> set, TDataEntry data)
+        {
+            foreach (var particle in set.Particles)
+                ApplyToParticle(particle, data);
+        }
+
+        public virtual void ApplyToParticle(TParticle particle, TDataEntry data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
