9d6752b9bff6265c181f95b44ba8cb401065cd46
Dmitrii Larionov <dlarionov51@gmail.com>
2022-12-10 02:12:37 +0300

    FluentModel now doesn't save models

--- FluentModel/FluentModel.cs
+++ FluentModel/FluentModel.cs
@@ -5,72 +5,45 @@ public class FluentModel<TMobileObject, TMap>
     where TMobileObject : IMobileObjectModel
     where TMap : IMapModel
 {
-    protected dynamic MotionModel { get; private set; }
-    protected dynamic ObservationModel { get; private set; }
-    protected TMobileObject MobileObject { get; private set; }
-    protected TMap Map { get; private set; }
+    protected virtual TMobileObject MobileObject { get; private set; }
+    protected virtual TMap Map { get; private set; }
+
+    public FluentModel(TMobileObject mobileObject, TMap map)
+    {
+        MobileObject = mobileObject;
+        Map = map;
+    }
 
     #region BaseMethods
-
-    public FluentModel<TMobileObject, TMap> SetMotionModel<TDataEntry>(
-        IMotionModel<TMobileObject, TDataEntry> motionModel)
-        where TDataEntry : struct
-    {
-        MotionModel = motionModel;
-        return this;
-    }
-
-
-    public FluentModel<TMobileObject, TMap> SetObservationModel<TDataEntry>(
-        IObservationModel<IMapModel, TDataEntry> observationModel)
-        where TDataEntry : struct
-    {
-        ObservationModel = observationModel;
-        return this;
-    }
-
-
-    public FluentModel<TMobileObject, TMap> UpdateMotionModel<TMotionModel, TDataEntry>(
-        TMotionModel model,
+    public FluentModel<TMobileObject, TMap> UpdateMotionModel<TDataEntry>(
+        IMotionModel<TMobileObject, TDataEntry> model,
         TDataEntry data)
-        where TMotionModel : IMotionModel<TMobileObject, TDataEntry>
         where TDataEntry : struct
     {
-        ApplyMotionModel<TMotionModel, TDataEntry>(model, data);
+        ApplyMotionModel<TDataEntry>(model, data);
         return this;
     }
 
-    public FluentModel<TMobileObject, TMap> UpdateMotionModel<TDataEntry>(TDataEntry data)
-        where TDataEntry : struct
-        => UpdateMotionModel(MotionModel, data);
-
-    public FluentModel<TMobileObject, TMap> UpdateObservationModel<TObservationModel, TDataEntry>(
-        TObservationModel model,
+    public FluentModel<TMobileObject, TMap> UpdateObservationModel<TDataEntry>(
+        IObservationModel<TMap, TDataEntry> model,
         TDataEntry data)
-        where TObservationModel : IObservationModel<TMap, TDataEntry>
         where TDataEntry : struct
     {
-        ApplyObservationModel<TObservationModel, TDataEntry>(model, data);
+        ApplyObservationModel<TDataEntry>(model, data);
         return this;
     }
-
-    public FluentModel<TMobileObject, TMap> UpdateObservationModel<TDataEntry>(TDataEntry data)
-        where TDataEntry : struct
-        => UpdateObservationModel(ObservationModel, data);
     #endregion
 
     #region VirtualMethods
-    protected virtual void ApplyMotionModel<TMotionModel, TDataEntry>(
-        TMotionModel model,
+    protected virtual void ApplyMotionModel<TDataEntry>(
+        IMotionModel<TMobileObject, TDataEntry> model,
         TDataEntry data)
-        where TMotionModel : IMotionModel<TMobileObject, TDataEntry>
         where TDataEntry : struct
     => model.Apply(MobileObject, data);
 
-    protected virtual void ApplyObservationModel<TObservationModel, TDataEntry>(
-        TObservationModel model,
+    protected virtual void ApplyObservationModel<TDataEntry>(
+        IObservationModel<TMap, TDataEntry> model,
         TDataEntry data)
-        where TObservationModel : IObservationModel<TMap, TDataEntry>
         where TDataEntry : struct
     => model.Apply(Map, data);
     #endregion
